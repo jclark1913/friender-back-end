57ad88a50c96660326a8a7a101a4c6a6f96a061f
Justin Clark <jclarksummit@gmail.com>
2023-05-02 18:55:47 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+from flask import (
+    Flask, render_template, request, flash, redirect, session, g, abort,
+)
+import boto3
+
+s3 = boto3.client('s3')
+
+
+
+load_dotenv()
+
+
+
+app = Flask(__name__)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+# app.config['SQLALCHEMY_ECHO'] = False
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+# app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+# toolbar = DebugToolbarExtension(app)
+
+# connect_db(app)
+
+########################
+
+@app.route("/upload", methods=["POST"])
+def upload_photo():
+    """Test s3 bucket"""
+    s3.upload_file("./testphoto.jpg", "friender-may-2023", "test.photos")
+

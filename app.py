e4dcde12ace23852cc2cc4c1eb25f91f2043d7c8
Justin Clark <jclarksummit@gmail.com>
2023-05-02 20:59:56 -0400

    Update boilerplate

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
 import os
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 from flask import (
     Flask, render_template, request, flash, redirect, session, g, abort,
 )
 import boto3
+from models import connect_db
 
 s3 = boto3.client('s3')
 
 
 
-#load_dotenv()
+load_dotenv()
 
 
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 # app.config['SQLALCHEMY_ECHO'] = False
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 # app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 # toolbar = DebugToolbarExtension(app)
 
-# connect_db(app)
+connect_db(app)
 
 ########################
 
